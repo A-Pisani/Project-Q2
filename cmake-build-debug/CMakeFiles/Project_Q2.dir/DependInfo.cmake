945d121871af0f306bb6b1323ab37f12b2e1f4c5
Alessandro Pisani <s269274@studenti.polito.it>
2020-11-09 11:51:56 +0100

    adding depend

---
+++ cmake-build-debug/CMakeFiles/Project_Q2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Alessandro-Pisani/Desktop/Project-Q2/main.c" "C:/Users/Alessandro-Pisani/Desktop/Project-Q2/cmake-build-debug/CMakeFiles/Project_Q2.dir/main.c.obj"
+  "C:/Users/Alessandro-Pisani/Desktop/Project-Q2/query_generators.c" "C:/Users/Alessandro-Pisani/Desktop/Project-Q2/cmake-build-debug/CMakeFiles/Project_Q2.dir/query_generators.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project_Q2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_Q2.dir/main.c.obj"
+  "CMakeFiles/Project_Q2.dir/query_generators.c.obj"
+  "Project_Q2.exe"
+  "Project_Q2.exe.manifest"
+  "Project_Q2.pdb"
+  "libProject_Q2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_Q2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
